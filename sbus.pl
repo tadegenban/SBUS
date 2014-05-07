6f29e9b65f53295bec3c9bc7e58586250a2c2a83
tadegenban <tadegenban@gmail.com>
2014-05-08 00:50:32 +0800

    test

--- sbus.pl
+++ sbus.pl
@@ -1,13 +1,43 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
+use Digest::SHA1 qw(sha1);
 
 # Documentation browser under "/perldoc"
 plugin 'PODRenderer';
 
 get '/' => sub {
-  my $self = shift;
-  $self->render('index');
+    my $self = shift;
+    my $echostr = $self->param('echostr');
+    my $pass = checkSignature($self);
+    if($pass){
+        $self->render($echostr);
+    }
+    else{
+        $self->render(text => $pass);
+    }
 };
+
+post '/' => sub {
+    my $self = shift;
+    say $self->req->body;
+};
+
+sub checkSignature{
+    my $self = shift;
+    my $signature = $self->param('signature');
+    my $timestamp = $self->param('timestamp');
+    my $nonce = $self->param('nonce');
+
+    my $token = 'tadetoken';
+    my $array = [$token, $timestamp, $nonce];
+    $array = [sort @$array];
+    my $str = join '', @$array;
+    $str = sha1($str);
+    if($signature eq $str){
+        return 1;
+    }
+    return 0;
+}
 
 app->start;
 __DATA__

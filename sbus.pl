2be67e767a2f51b00ac34beeb0f8f585757f2951
tadegenban <tadegenban@gmail.com>
2014-05-15 22:56:22 +0800

    test

--- sbus.pl
+++ sbus.pl
@@ -4,10 +4,15 @@ use Mojo::DOM;
 use Digest::SHA1 qw(sha1);
 use Data::Dumper;
 use Encode;
-
+use utf8;
+use Text::CSV;
+use Data::Dumper;
+binmode(STDOUT, ":utf8");
 # Documentation browser under "/perldoc"
 plugin 'PODRenderer';
 
+my $schedule_file = 'schedule.csv';
+my $schedule_hash = load_schedule($schedule_file);
 get '/' => sub {
     my $self = shift;
     my $echostr = $self->param('echostr');
@@ -68,9 +73,8 @@ sub response{
         $response = get_help();
         return $response;
     }
-    if($content eq '张江'){
-        my $loc = "张江";
-        $response = get_schedule($loc);
+    else{
+        $response = get_schedule($content);
         return $response;
     }
     return $response;
@@ -81,9 +85,66 @@ sub get_help{
 }
 sub get_schedule{
     my $loc = shift;
-    if($loc eq '张江'){
-        return '高科';
+    if($loc =~ /张江/){
+        my $station = '张江';
+        my $response = parse_schedule($schedule_hash, 'workday', $station);
+        return $response;
     }
+    if($loc =~ /龙阳/){
+        my $station = '龙阳';
+        my $response = parse_schedule($schedule_hash, 'workday', $station);
+        return $response;
+    }
+}
+sub load_schedule{
+    my $file = shift;
+    open my $fh, '<:encoding(utf8)', $file;
+    my $csv = Text::CSV->new();
+    my $hash = {};
+    my $timing;
+    my $station_A;
+    my $station_B;
+    my $flag_head = 0;
+    while(my $row = $csv->getline($fh)){
+        if($row->[0] eq '$'){
+            $timing = $row->[1];
+            $flag_head   = 1;
+            next;
+        }
+        if(1 == @$row){
+            $flag_head = 1;
+            next;
+        }
+        if($flag_head == 1){
+            $station_A = $row->[0];
+            $station_B = $row->[1];
+            $hash->{$timing}->{$station_A} = [$row] unless exists $hash->{$timing}->{$station_A};
+            $flag_head = 0;
+            next;
+        }
+        if($flag_head == 0){
+            my $time_A = $row->[0];
+            my $time_B = $row->[1];
+            push $hash->{$timing}->{$station_A}, $row;
+            next;
+        }
+    }
+    return $hash;
+}
+sub parse_schedule{
+    my ($schedule_hash, $timing, $station) = @_;
+    my $array = $schedule_hash->{$timing}->{$station};
+    my $response;
+    foreach my $arr(@$array){
+        if($arr->[0] eq '='){
+            $response .= "==========\n";
+        }
+        else{
+            $response .= sprintf("%-8s | %-8s", @$arr);
+            $response .= "\n";
+        }
+    }
+    return $response;
 }
 app->start;
 __DATA__
